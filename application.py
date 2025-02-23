b18741d5b6451a6da6ad1c9d4856f4bf78eccc36
Divyanshu-Mathur <divyanshumathur2004@gmail.com>
2025-02-23 14:37:33 +0530

    forth

--- application.py
+++ application.py
@@ -44,4 +44,6 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Railway provides a PORT env variable
+    print("Files in model directory:", os.listdir('model'))
+    app.run(host="0.0.0.0", port=port, debug=True)
